7756bc73758eb343313bb20be393c3b4121bfe88
lgao1024 <lgao1024@outlook.com>
2026-03-05 00:30:00 +0800

    layout 10.1

--- js/lei-blog.js
+++ js/lei-blog.js
@@ -41,35 +41,20 @@ $(document).ready(function() {
     $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
-// Navigation Scripts to Show Header on Scroll-Up
+// Navigation script: keep navbar pinned on top and only handle side catalog position
 jQuery(document).ready(function($) {
     var MQL = 1170;
+    $('.navbar-custom').addClass('is-fixed is-visible');
 
-    //primary navigation slide-in effect
+    // keep side catalog behavior on large screens
     if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height(),
-            bannerHeight  = $('.intro-header .container').height();     
+        var bannerHeight  = $('.intro-header .container').height();     
         $(window).on('scroll', {
                 previousTop: 0
             },
             function() {
                 var currentTop = $(window).scrollTop(),
                     $catalog = $('.side-catalog');
-
-                //check if user is scrolling up by mouse or keyborad
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
 
 
                 //adjust the appearance of side-catalog
